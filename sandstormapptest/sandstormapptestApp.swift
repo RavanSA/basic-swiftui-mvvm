c16e0a2f1844bac899bc2a90017fcc8e43eab2d0
Revan SADIGLI <revan.sadiqli99@gmail.com>
2024-02-17 23:48:56 +0300

    Initial Commit

---
+++ sandstormapptest/sandstormapptestApp.swift
@@ -0,0 +1,17 @@
+//
+//  sandstormapptestApp.swift
+//  sandstormapptest
+//
+//  Created by Revan SADIGLI on 17.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct sandstormapptestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
